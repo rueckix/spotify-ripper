4f65490ace709b1c2129ddafa85dc441f421e8bc
rueckix <39458989+rueckix@users.noreply.github.com>
2019-09-07 16:47:55 +0200

    Update entrypoint.sh
    use stdout for logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spotify-ripper -S /config/ -k /config/spotify_appkey.key -d /music -u ${user} -p ${pass} ${playlist} && python /ripper/source-code/spotify_ripper/emptyPlaylist.py
+spotify-ripper -S /config/ -k /config/spotify_appkey.key -d /music -u ${user} -p ${pass} ${playlist} -L - && python /ripper/source-code/spotify_ripper/emptyPlaylist.py
