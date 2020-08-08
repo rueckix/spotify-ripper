6664a4745c5e319b4c5a7bcd5191ef023d04ea4c
rueckix <39458989+rueckix@users.noreply.github.com>
2020-08-08 12:37:46 +0200

    Update entrypoint.sh
    -S is not working, going back to /root/.spotify-ripper (default)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spotify-ripper -S /config/ -k /config/spotify_appkey.key -d /music -u ${user} -p ${pass} ${playlist} -L - && python /ripper/source-code/spotify_ripper/emptyPlaylist.py
+spotify-ripper  -k /config/spotify_appkey.key -d /music -u ${user} -p ${pass} ${playlist} -L - && python /ripper/source-code/spotify_ripper/emptyPlaylist.py
